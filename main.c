2cc22940de79ce9c9288fb20e84621f54c5a8b74
alkut <alexseykutasov@gmail.com>
2022-09-28 15:22:52 +0300

    change main

--- main.c
+++ main.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
+#include <math.h>
 #include "interpolation2.h"
 
 int main()
 {
     int nx = 50, ny = 50;
-    double x = 4.18, y = 3.76845;
+    double x = 0.18, y = 0.76845;
 	interpolation2_ctx	ctx;
 
 	ctx = interpolation2_create(INTERPOLATION2_METHOD1,
-				    nx, ny, 6, -3.5, 8.1, 2.8, 4.9);
+				    nx, ny, 6, -0.5, 0.8, -1.1, 0.9);
 
 	printf("val: %lf\n", interpolation2_calculate(ctx, x, y));
 
@@ -16,9 +17,11 @@ int main()
     interpolation2_ctx	ctx2;
 
     ctx2 = interpolation2_create(INTERPOLATION2_METHOD2,
-                                nx, ny, 4, -3.5, 8.1, 2.8, 4.9);
+                                nx, ny, 6, -0.5, 0.8, -1.1, 0.9);
 
     printf("val: %lf\n", interpolation2_calculate(ctx2, x, y));
+
+    printf("true val: %lf\n", exp(x * x - y * y));
 
 	interpolation2_destroy(ctx);
     interpolation2_destroy(ctx2);

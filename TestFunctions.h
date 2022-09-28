727bf477ce6beac734400d0f6e8495146d1c8760
alkut <alexseykutasov@gmail.com>
2022-09-28 17:56:51 +0300

    add test functions

--- TestFunctions.h
+++ TestFunctions.h
@@ -2,82 +2,82 @@
 
 static double f0(double x, double y)
 {
-    return x * y;
+    return 1.0;
 }
 
 static double f0x(double x, double y)
 {
-    return y;
+    return 0.0;
 }
 
 static double f0y(double x, double y)
 {
-    return x;
+    return 0.0;
 }
 
 static double f0xy(double x, double y)
 {
-    return 1.0;
+    return 0.0;
 }
 
 static double f1(double x, double y)
 {
-    return x * y;
+    return x;
 }
 
 static double f1x(double x, double y)
 {
-    return y;
+    return 1.0;
 }
 
 static double f1y(double x, double y)
 {
-    return x;
+    return 0.0;
 }
 
 static double f1xy(double x, double y)
 {
-    return 1.0;
+    return 0.0;
 }
 
 static double f2(double x, double y)
 {
-    return x * y;
+    return y;
 }
 
 static double f2x(double x, double y)
 {
-    return y;
+    return 0.0;
 }
 
 static double f2y(double x, double y)
 {
-    return x;
+    return 1.0;
 }
 
 static double f2xy(double x, double y)
 {
-    return 1.0;
+    return 0.0;
 }
 
 static double f3(double x, double y)
 {
-    return x * y;
+    return x + y;
 }
 
 static double f3x(double x, double y)
 {
-    return y;
+    return 1.0;
 }
 
 static double f3y(double x, double y)
 {
-    return x;
+    return 1.0;
 }
 
 static double f3xy(double x, double y)
 {
-    return 1.0;
+    return 0.0;
 }
 
 static double f4(double x, double y)
@@ -104,22 +104,22 @@ static double f4xy(double x, double y)
 
 static double f5(double x, double y)
 {
-    return x * y;
+    return x * x + y * y;
 }
 
 static double f5x(double x, double y)
 {
-    return y;
+    return 2.0 * x;
 }
 
 static double f5y(double x, double y)
 {
-    return x;
+    return 2.0 * y;
 }
 
 static double f5xy(double x, double y)
 {
-    return 1.0;
+    return 0.0;
 }
 
 static double f6(double x, double y)
@@ -144,20 +144,23 @@ static double f6xy(double x, double y)
 
 static double f7(double x, double y)
 {
-    return x * y;
+    return 1.0 / (25.0 * (x * x + y * y) + 1.0);
 }
 
 static double f7x(double x, double y)
 {
-    return y;
+    double tmp = 25.0 * (x * x + y * y) + 1.0;
+    return -50.0 * x / (tmp * tmp);
 }
 
 static double f7y(double x, double y)
 {
-    return x;
+    double tmp = 25.0 * (x * x + y * y) + 1.0;
+    return -50.0 * y / (tmp * tmp);
 }
 
 static double f7xy(double x, double y)
 {
-    return 1.0;
+    double tmp = 25.0 * (x * x + y * y) + 1.0;
+    return 5000.0 * y * x / (tmp * tmp * tmp);
 }
--- main.c
+++ main.c
@@ -1,15 +1,18 @@
 #include <stdio.h>
 #include <math.h>
 #include "interpolation2.h"
+#include "TestFunctions.h"
 
 int main()
 {
-    int nx = 50, ny = 50;
-    double x = 0.76845, y = 0.08;
+    const double (*Functions[8])(double, double) = {f0, f1, f2, f3, f4, f5, f6, f7};
+    int nx = 50, ny = 50, k = 6;
+    double x = 0.76845, y = 0.08, x_a = -0.5, x_b = 0.8, y_a = -1.1, y_b = 0.9;
+
 	interpolation2_ctx	ctx;
 
 	ctx = interpolation2_create(INTERPOLATION2_METHOD1,
-				    nx, ny, 6, -0.5, 0.8, -1.1, 0.9);
+				    nx, ny, k, x_a, x_b, y_a, y_b);
 
 	printf("val1: %lf\n", interpolation2_calculate(ctx, x, y));
 
@@ -17,11 +20,11 @@ int main()
     interpolation2_ctx	ctx2;
 
     ctx2 = interpolation2_create(INTERPOLATION2_METHOD2,
-                                nx, ny, 6, -0.5, 0.8, -1.1, 0.9);
+                                nx, ny, k, x_a, x_b, y_a, y_b);
 
     printf("val2: %lf\n", interpolation2_calculate(ctx2, x, y));
 
-    printf("true val: %lf\n", exp(x * x - y * y));
+    printf("true val: %lf\n", Functions[k](x, y));
 
 	interpolation2_destroy(ctx);
     interpolation2_destroy(ctx2);
